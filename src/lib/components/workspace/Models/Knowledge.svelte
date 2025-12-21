00865bc11deea871eaa9f432a91a1b1a6ccee40b
Darío Ruellan <dario.ruellan@gmail.com>
2025-12-21 12:38:10 -0300

    Feat: add Google Drive file upload integration to features that allow file upload

--- src/lib/components/workspace/Models/Knowledge.svelte
+++ src/lib/components/workspace/Models/Knowledge.svelte
@@ -7,6 +7,7 @@
 
 	import { getKnowledgeBases } from '$lib/apis/knowledge';
 	import { uploadFile } from '$lib/apis/files';
+	import { createPicker, getAuthToken } from '$lib/utils/google-drive-picker';
 
 	import { toast } from 'svelte-sonner';
 	import { v4 as uuidv4 } from 'uuid';
@@ -127,6 +128,32 @@
 		});
 	};
 
+	const uploadGoogleDriveHandler = async () => {
+		try {
+			try {
+				const fileData = await createPicker();
+				if (fileData) {
+					const file = new File([fileData.blob], fileData.name, {
+						type: fileData.blob.type
+					});
+					await uploadFileHandler(file);
+				} else {
+					console.log('No file was selected from Google Drive');
+				}
+			} catch (error) {
+				console.error('Google Drive Error:', error);
+				toast.error(
+					$i18n.t('Error accessing Google Drive: {{error}}', {
+						error: error.message
+					})
+				);
+			}
+		} catch (error) {
+			console.error('Google Drive initialization error:', error);
+			toast.error($i18n.t('Failed to initialize Google Drive'));
+		}
+	};
+
 	onMount(async () => {
 		if (!$knowledge) {
 			knowledge.set(await getKnowledgeBases(localStorage.token));
@@ -220,6 +247,42 @@
 							filesInputElement.click();
 						}}>{$i18n.t('Upload Files')}</button
 					>
+				{/if}
+
+				{#if $config?.features?.enable_google_drive_integration && ($user?.role === 'admin' || $user?.permissions?.chat?.file_upload)}
+					<button
+						class=" px-3.5 py-1.5 font-medium hover:bg-black/5 dark:hover:bg-white/5 outline outline-1 outline-gray-100 dark:outline-gray-850 rounded-3xl flex items-center gap-2"
+						type="button"
+						on:click={uploadGoogleDriveHandler}
+					>
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 87.3 78" class="w-4">
+							<path
+								d="m6.6 66.85 3.85 6.65c.8 1.4 1.95 2.5 3.3 3.3l13.75-23.8h-27.5c0 1.55.4 3.1 1.2 4.5z"
+								fill="#0066da"
+							/>
+							<path
+								d="m43.65 25-13.75-23.8c-1.35.8-2.5 1.9-3.3 3.3l-25.4 44a9.06 9.06 0 0 0 -1.2 4.5h27.5z"
+								fill="#00ac47"
+							/>
+							<path
+								d="m73.55 76.8c1.35-.8 2.5-1.9 3.3-3.3l1.6-2.75 7.65-13.25c.8-1.4 1.2-2.95 1.2-4.5h-27.502l5.852 11.5z"
+								fill="#ea4335"
+							/>
+							<path
+								d="m43.65 25 13.75-23.8c-1.35-.8-2.9-1.2-4.5-1.2h-18.5c-1.6 0-3.15.45-4.5 1.2z"
+								fill="#00832d"
+							/>
+							<path
+								d="m59.8 53h-32.3l-13.75 23.8c1.35.8 2.9 1.2 4.5 1.2h50.8c1.6 0 3.15-.45 4.5-1.2z"
+								fill="#2684fc"
+							/>
+							<path
+								d="m73.4 26.5-12.7-22c-.8-1.4-1.95-2.5-3.3-3.3l-13.75 23.8 16.15 28h27.45c0-1.55-.4-3.1-1.2-4.5z"
+								fill="#ffba00"
+							/>
+						</svg>
+						<span>{$i18n.t('Google Drive')}</span>
+					</button>
 				{/if}
 			</div>
 		{/if}
